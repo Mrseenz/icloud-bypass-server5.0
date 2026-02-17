d2dc888600852bd061fb05ab0f70f1c371c918a2
Mrseenz <101962919+Mrseenz@users.noreply.github.com>
2026-02-17 09:11:38 +0200

    Add dual-IMEI support and XAMPP setup automation

--- deviceservices/tools/rename-requests.php
+++ deviceservices/tools/rename-requests.php
@@ -1,34 +1,39 @@
 <?php
-foreach (glob("requests/*.xml") as $filename) 
-{
-    try
-	{
-		$requestfile = file_get_contents($filename);
-		// load and decode activation info 
-		$encodedrequest = new DOMDocument;
-		$encodedrequest->loadXML($requestfile);
-		$activationDecoded= base64_decode($encodedrequest->getElementsByTagName('data')->item(0)->nodeValue);
+foreach (glob(__DIR__ . '/requests/*.xml') as $filename) {
+    try {
+        $requestfile = file_get_contents($filename);
+        $encodedrequest = new DOMDocument();
+        $encodedrequest->loadXML($requestfile);
+        $activationDecoded = base64_decode($encodedrequest->getElementsByTagName('data')->item(0)->nodeValue, true);
 
-		$decodedrequest = new DOMDocument;
-		$decodedrequest->loadXML($activationDecoded);
-		$nodes = $decodedrequest->getElementsByTagName('dict')->item(0)->getElementsByTagName('*');
+        if ($activationDecoded === false) {
+            throw new RuntimeException('Invalid activation payload');
+        }
 
-		for ($i = 0; $i < $nodes->length - 1; $i=$i+2)
-		{
-			switch ($nodes->item($i)->nodeValue)
-			{
-				case "ProductType": $productType = $nodes->item($i + 1)->nodeValue; break;
-				case "ProductVersion": $productVersion = $nodes->item($i + 1)->nodeValue; break;
-				case "SerialNumber": $serialNumber = $nodes->item($i + 1)->nodeValue; break;
-			}
-		}
+        $decodedrequest = new DOMDocument();
+        $decodedrequest->loadXML($activationDecoded);
+        $nodes = $decodedrequest->getElementsByTagName('dict')->item(0)->getElementsByTagName('*');
 
-		echo basename($filename).' -><br/>';
-		$newfilename = str_replace(',', '.', $productType).'_ios'.$productVersion.'_'.$serialNumber.'.xml';
-		echo '<b style="color: green">'.$newfilename.'</b>';
-		echo ' ...................................................................... <b>'.
-				(rename($filename, 'requests/'.$newfilename) ? 'OK' : 'ERROR').'</b>';
-	} catch (Exception $e) {	echo 'Could not parse request file <b>'.basename($filename).'</b>';}
-	echo '<br/>------------------------------------------------------------------------------<br/>';
+        $productType = 'Unknown';
+        $productVersion = 'Unknown';
+        $serialNumber = 'Unknown';
+
+        for ($i = 0; $i < $nodes->length - 1; $i += 2) {
+            switch ($nodes->item($i)->nodeValue) {
+                case 'ProductType': $productType = $nodes->item($i + 1)->nodeValue; break;
+                case 'ProductVersion': $productVersion = $nodes->item($i + 1)->nodeValue; break;
+                case 'SerialNumber': $serialNumber = $nodes->item($i + 1)->nodeValue; break;
+            }
+        }
+
+        echo htmlspecialchars(basename($filename), ENT_QUOTES, 'UTF-8') . ' -><br/>';
+        $newfilename = str_replace(',', '.', $productType) . '_ios' . $productVersion . '_' . $serialNumber . '.xml';
+        echo '<b style="color: green">' . htmlspecialchars($newfilename, ENT_QUOTES, 'UTF-8') . '</b>';
+        echo ' ...................................................................... <b>' .
+            (rename($filename, __DIR__ . '/requests/' . $newfilename) ? 'OK' : 'ERROR') . '</b>';
+    } catch (Exception $e) {
+        echo 'Could not parse request file <b>' . htmlspecialchars(basename($filename), ENT_QUOTES, 'UTF-8') . '</b>';
+    }
+    echo '<br/>------------------------------------------------------------------------------<br/>';
 }
 ?>
